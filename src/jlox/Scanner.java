2d43a6b2fba718cff7f6616a1f9a612fca8dbf5e
Guilherme Araujo <guilherme00.ge@gmail.com>
2025-06-17 23:42:07 -0300

    scantoken

--- src/jlox/Scanner.java
+++ src/jlox/Scanner.java
@@ -10,7 +10,7 @@ import static TokenType.*;
 class Scanner {
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
-private int start = 0;
+  private int start = 0;
   private int current = 0;
   private int line = 1;
 
@@ -29,5 +29,33 @@ private int start = 0;
   }
    private boolean isAtEnd() {
     return current >= source.length();
+  }
+  
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+      case '(': addToken(LEFT_PAREN); break;
+      case ')': addToken(RIGHT_PAREN); break;
+      case '{': addToken(LEFT_BRACE); break;
+      case '}': addToken(RIGHT_BRACE); break;
+      case ',': addToken(COMMA); break;
+      case '.': addToken(DOT); break;
+      case '-': addToken(MINUS); break;
+      case '+': addToken(PLUS); break;
+      case ';': addToken(SEMICOLON); break;
+      case '*': addToken(STAR); break; 
+}
+  }
+   private char advance() {
+    return source.charAt(current++);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
   }
 }
