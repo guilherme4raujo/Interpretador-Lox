1a2138b9d580bf3ee995e1dbe6f9107ed02a64de
Guilherme Araujo <guilherme00.ge@gmail.com>
2025-06-18 00:06:02 -0300

    comentarios

--- src/jlox/Scanner.java
+++ src/jlox/Scanner.java
@@ -57,11 +57,32 @@ class Scanner {
       case '>':
         addToken(match('=') ? GREATER_EQUAL : GREATER);
         break;
+        case '/':
+        if (match('/')) {
+          // A comment goes until the end of the line.
+          while (peek() != '\n' && !isAtEnd()) advance();
+        } else {
+          addToken(SLASH);
+        }
+        break;
+      case ' ':
+      case '\r':
+      case '\t':
+        // Ignore whitespace.
+        break;
+
+      case '\n':
+        line++;
+        break;
       
       default:
         Lox.error(line, "Unexpected character.");
         break;
 }
+  }
+   private char peek() {
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
   }
    private char advance() {
     return source.charAt(current++);
